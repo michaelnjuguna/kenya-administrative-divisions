26432eb9498eba592925da7ac6797867e9995a09
michael njuguna <michaelnjuguna184@gmail.com>
2023-04-15 07:56:29 +0300

    implemented get counties

--- index.js
+++ index.js
@@ -24,28 +24,68 @@ readCountyData()
   });
 
 // getAll
-exports.getAll = ()=>{
-    readCountyData().then((data)=>{
-        console.log(data)
-        return data;
+exports.getAll = () => {
+  readCountyData()
+    .then((data) => {
+      console.log(data);
+      return data;
     })
     .catch((error) => {
-        console.log(error);
-        return;
-      })
+      console.log(error);
+      return;
+    });
 };
 
-
 // getCounty
-readCountyData().then((data)=>{
-    let input = 1;
-    if(!input || input === "" || !input.length){
-        for(let i = 0;i<47;i++){
-            console.log(data[i].county_name)
-        }
-    }else if(input.typ){
-        
+exports.getCounty = (input)=>{
+readCountyData().then((data) => {
+  // county code or name
+  let result = [];
+  // when input is empty
+  if (
+    typeof input === "undefined" ||
+    input === null ||
+    input === "" ||
+    input === 0 ||
+    input.length === 0
+  ) {
+    for (let i = 0; i < 47; i++) {
+      result.push({
+        county_code: data[i].county_code,
+        county_name: data[i].county_name,
+      });
     }
-})
+    // when result is a number
+  } else if (
+    (input > 0 && input < 48 && typeof input === "number") ||
+    (typeof +input === "number" && !isNaN(+input) && +input > 0 && +input < 48)
+  ) {
+    input = input - 1;
+    result.push({
+      county_code: data[input].county_code,
+      county_name: data[input].county_name,
+      constituencies: data[input].constituencies,
+    });
+    // when result is a string
+  } else if (typeof input === "string") {
+    for (let i = 0; i < 47; i++) {
+      if (data[i].county_name.toLowerCase() === input.toLowerCase()) {
+        result.push({
+          county_code: data[i].county_code,
+          county_name: data[i].county_name,
+          constituencies: data[i].constituencies,
+        });
+      }
+    }
+  } else if (result.length === 0) {
+    result = "Invalid input";
+  } else {
+    result = "Invalid input";
+  }
+  console.log(result);
+  return result;
+})};
 // getConstituencies
+
+
 // getWards
