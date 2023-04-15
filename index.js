5aeedb7c073995646adced1a7941ed19c3f25bbf
michael njuguna <michaelnjuguna184@gmail.com>
2023-04-15 08:56:56 +0300

    implemented get constituencies

--- index.js
+++ index.js
@@ -37,55 +37,117 @@ exports.getAll = () => {
 };
 
 // getCounty
-exports.getCounty = (input)=>{
-readCountyData().then((data) => {
-  // county code or name
-  let result = [];
-  // when input is empty
-  if (
-    typeof input === "undefined" ||
-    input === null ||
-    input === "" ||
-    input === 0 ||
-    input.length === 0
-  ) {
-    for (let i = 0; i < 47; i++) {
-      result.push({
-        county_code: data[i].county_code,
-        county_name: data[i].county_name,
-      });
-    }
-    // when result is a number
-  } else if (
-    (input > 0 && input < 48 && typeof input === "number") ||
-    (typeof +input === "number" && !isNaN(+input) && +input > 0 && +input < 48)
-  ) {
-    input = input - 1;
-    result.push({
-      county_code: data[input].county_code,
-      county_name: data[input].county_name,
-      constituencies: data[input].constituencies,
-    });
-    // when result is a string
-  } else if (typeof input === "string") {
-    for (let i = 0; i < 47; i++) {
-      if (data[i].county_name.toLowerCase() === input.toLowerCase()) {
+exports.getCounty = (input) => {
+  readCountyData()
+    .then((data) => {
+      // county code or name
+      let result = [];
+      // when input is empty
+      if (
+        typeof input === "undefined" ||
+        input === null ||
+        input === "" ||
+        input === 0 ||
+        input.length === 0
+      ) {
+        for (let i = 0; i < 47; i++) {
+          result.push({
+            county_code: data[i].county_code,
+            county_name: data[i].county_name,
+          });
+        }
+        // when result is a number
+      } else if (
+        (input > 0 && input < 48 && typeof input === "number") ||
+        (typeof +input === "number" &&
+          !isNaN(+input) &&
+          +input > 0 &&
+          +input < 48)
+      ) {
+        input = input - 1;
         result.push({
-          county_code: data[i].county_code,
-          county_name: data[i].county_name,
-          constituencies: data[i].constituencies,
+          county_code: data[input].county_code,
+          county_name: data[input].county_name,
+          constituencies: data[input].constituencies,
         });
+        // when result is a string
+      } else if (typeof input === "string") {
+        for (let i = 0; i < 47; i++) {
+          if (data[i].county_name.toLowerCase() === input.toLowerCase()) {
+            result.push({
+              county_code: data[i].county_code,
+              county_name: data[i].county_name,
+              constituencies: data[i].constituencies,
+            });
+          }
+        }
+      } else if (result.length === 0) {
+        result = "Invalid input";
+      } else {
+        result = "Invalid input";
       }
-    }
-  } else if (result.length === 0) {
-    result = "Invalid input";
-  } else {
-    result = "Invalid input";
-  }
-  console.log(result);
-  return result;
-})};
+      console.log(result);
+      return result;
+    })
+    .catch((error) => {
+      console.log(error);
+      return;
+    });
+};
 // getConstituencies
-
-
+exports.getConstituencies = (input) => {
+  readCountyData()
+    .then((data) => {
+      let result = [];
+      // when input is empty
+      if (
+        input === "" ||
+        input === null ||
+        input === undefined ||
+        input === 0 ||
+        input.length === 0
+      ) {
+        for (let i = 0; i < 47; i++) {
+          for (let j = 0; j < data[i].constituencies.length; j++) {
+            result.push({
+              county_name: data[i].county_name,
+              county_code: data[i].county_code,
+              constituency_name: data[i].constituencies[j].constituency_name,
+            });
+          }
+        }
+        // when input is a string
+      } else if (typeof input === "string") {
+        for (let i = 0; i < 47; i++) {
+          for (let j = 0; j < data[i].constituencies.length; j++) {
+            if (
+              data[i].constituencies[j].constituency_name.toLowerCase() ===
+              input.toLowerCase()
+            ) {
+              result.push({
+                county_name: data[i].county_name,
+                county_code: data[i].county_code,
+                constituency_name: data[i].constituencies[j].constituency_name,
+                wards: data[i].constituencies[j].wards,
+              });
+              break;
+            }
+          }
+          // when result is not empty
+          if (result.length > 0) {
+            break;
+          }
+        }
+      }
+      if (result.length === 0) {
+        result = "Invalid input";
+      }
+      console.log(result);
+      return result;
+    })
+    .catch((error) => {
+      console.log(error);
+      return;
+    });
+};
 // getWards
