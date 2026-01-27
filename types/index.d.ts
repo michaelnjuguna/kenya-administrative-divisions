b3ee08dac34eba629ebd381e53a585affbabc02f
Davy Kamanzi <davykamanzi@gmail.com>
2026-01-27 19:01:05 +0300

    Simplifying types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,23 +1,19 @@
-interface WardInfo{
-    county_name: string;
-    county_code: number;
-    constituency_name: string;
-    ward_name: string;
-}
-interface ConstituencyInfo{
-    county_name: string;
-    county_code: number;
-    constituency_name: string;
-    wards?: string[];
-}
-interface CountyInfo{
-    county_name: string;
-    county_code: number;
-    constituencies?: ConstituencyInfo[];
+export interface WardInfo {
+	county_name: string;
+	county_code: number;
+	constituency_name: string;
+	ward_name: string;
 }
 
-export declare function getAll(): Promise<CountyInfo[]>;
-export declare function getCounties(input?: number | string): Promise<CountyInfo[]>;
-export declare function getConstituencies(input?: number | string): Promise<ConstituencyInfo[]>;
-export declare function getWards(input?: number | string): Promise<WardInfo[] | string>;
+export interface ConstituencyInfo {
+	county_name: string;
+	county_code: number;
+	constituency_name: string;
+	wards?: string[];
+}
 
+export interface CountyInfo {
+	county_name: string;
+	county_code: number;
+	constituencies?: ConstituencyInfo[];
+}
